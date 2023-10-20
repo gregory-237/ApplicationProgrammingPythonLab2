38b1005a9e2cd356886d3be0323f81e78c2e100c
Григорий Петров <petrovgrigorij29@gmail.com>
2023-10-21 01:18:15 +0400

    Made second and third script using pandas

--- script2.py
+++ script2.py
@@ -1,19 +1,25 @@
-dates_courses = []
-f = open("dataset.csv", 'r')
-for row in f:
-    dates_courses.append(row.replace("\n", "").replace("-", ""))
-dates_courses.reverse()
-list_dates = []
-for i in range (1, len(dates_courses)):
-    if dates_courses[i-1][:4] == dates_courses[i][:4]:
-        list_dates.append(dates_courses[i-1])
-    else:
-        list_dates.append(dates_courses[i-1])
-        with open(f"script2/{list_dates[0][:8]}_{list_dates[-1][:8]}.csv", 'w') as d:
-            for row in list_dates:
-                d.write(f'{row[:4]}/{row[4:6]}/{row[6:8]}{row[8:]}\n')
-            list_dates = []
-        continue
+import pandas as pd
+
+df = pd.read_csv('dataset.csv', parse_dates=['Date'], names=['Date', 'Dollar_Rate'])
+df['Date'] = pd.to_datetime(df['Date'])
+df = df.sort_values(by='Date')
+years_data = []
+for name, group in df.set_index('Date').groupby(pd.Grouper(freq='Y')):
+    years_data.append(group.apply(lambda x: [str(x.name)[:10], str(x['Dollar_Rate'])], axis=1).values.tolist())
+
+
+def split_by_years(years_data_list):
+    """Splitting file by years"""
+    for year in years_data_list:
+        with open(f'script2/{year[0][0].replace("-", "")}_{year[-1][0].replace("-", "")}.csv', 'w') as f:
+            for day in year:
+                f.write(f'{day[0]},{day[1]}\n')
+
+
+if __name__ == "__main__":
+    split_by_years(years_data)
+
+
 
 
 
